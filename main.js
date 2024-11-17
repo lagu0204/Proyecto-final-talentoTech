33e093215c1a8380d7602ca8d5362b027e7db042
JuaN Diego <juadiegogarcialaguna@gmail.com>
2024-11-16 19:28:30 -0500

    Portafolio

---
+++ main.js
@@ -0,0 +1,5 @@
+const btn = document.querySelector('.btn');  // Seleccionamos el div con la clase .btn
+
+    btn.addEventListener('click', function() {
+        window.location.href = 'https://github.com/lagu0204/dashboard.git';
+    });
